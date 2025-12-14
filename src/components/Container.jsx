fe4bc6818f3a7b1d4a05e86166f01e9697c5ffdd
sobita <srdsobitar@gmail.com>
2025-12-15 00:24:25 +0600

    add react icon

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children, className}) => {
   return (
-    <div className={`max-w-[1170px] m-auto ${className}`}>{children}</div>
+    <div ></div>
   )
 }
 
